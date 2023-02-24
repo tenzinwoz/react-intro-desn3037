0cba43cf2cde3aac8b14bfee2d9836d24d2b4228
tenzin woeser <tenzinwoeser15@gmail.com>
2023-02-23 23:52:11 -0500

    added page contact

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import Client from "./components/Client";
 import Contact from "./components/Contact";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Box, Container } from "@mui/system";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Menu />
-        <Routes>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/client" element={<Client />} />
-          <Route exact path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
+      <Container maxWidth="sm" sx={{ mt: 4 }}>
+        <Router>
+          <Box sx={{ mb: 3 }}>
+            <Menu />
+          </Box>
+
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/client" element={<Client />} />
+            <Route exact path="/contact" element={<Contact />} />
+          </Routes>
+        </Router>
+      </Container>
     </div>
   );
 }
